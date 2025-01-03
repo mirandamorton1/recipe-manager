cf5c574f7631a8ede0e74e81e178439b7397afbb
mirandamorton1 <107001559+mirandamorton1@users.noreply.github.com>
2025-01-03 17:27:29 -0600

    initial styling of landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,45 @@
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../styles/LandingPages.module.scss";
 
-const Index = () => (
-  <div>
-    <h1>Welcome!</h1>
-    <Link href="/signup">
-      <button>Sign Up</button>
-    </Link>
-    <Link href="/login">
-      <button>Login</button>
-    </Link>
-  </div>
-);
+const IndexPage = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <h1>The Recipe Box</h1>
+        <p>All of Your Recipes, All in One Place.</p>
+      </div>
 
-export default Index;
+      <div className={styles.center}>
+        <Image
+          src="/images/food.png"
+          alt="Food image"
+          className={styles.foodImage}
+          width={400}
+          height={400}
+        ></Image>
+      </div>
+
+      <div className={styles.right}>
+        <div className={styles.buttonsWrapper}>
+          <div className={styles.buttonGroup}>
+            <p className={styles.label}>New User?</p>
+            <Link href="/signup" className={styles.button}>
+              Sign Up
+            </Link>
+          </div>
+
+          <div className={styles.buttonGroup}>
+            <p className={styles.label}>Returning?</p>
+            <Link href="/login" className={styles.button}>
+              Login
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default IndexPage;
